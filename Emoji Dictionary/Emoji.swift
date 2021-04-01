d4fa56185dfc2ae73e197ffea049bdf92acea65c
Justin Storrer <storrerjustin@gmail.com>
2021-03-31 21:49:13 -0400

    Added a new swift file to create a new structure that holds emoji's information

---
+++ Emoji Dictionary/Emoji.swift
@@ -0,0 +1,16 @@
+//
+//  Emoji.swift
+//  Emoji Dictionary
+//
+//  Created by Justin Storrer on 3/31/21.
+//
+
+import Foundation
+
+struct Emoji {
+    var character = ""
+    var defintion = ""
+    var year = 0
+    var rating = 0.0
+    var category = ""
+}
